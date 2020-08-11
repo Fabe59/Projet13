03af554920c2a1a82711db034bf8c64c9f65c1e9
Fabe59 <pringuetfabrice@gmail.com>
2020-08-11 17:02:52 +0200

    dynamic centering updated in leafletmap.js

--- openrider/static/js/leafletmap.js
+++ openrider/static/js/leafletmap.js
@@ -14,7 +14,7 @@ let result_elt = document.querySelectorAll('.result_elt');
 for (var i = 0; i < result_elt.length; i++) {
     let lat = result_elt[i].querySelector('.lat').textContent;
     let lon = result_elt[i].querySelector('.lon').textContent;
-    let name = result_elt[i].querySelector('.name').textContent;
+    //let name = result_elt[i].querySelector('.name').textContent;
     coord = [lat, lon];
     bounds.push(coord)
     L.marker(coord).setLatLng(coord).addTo(map)
@@ -25,7 +25,13 @@ for (var i = 0; i < result_elt.length; i++) {
   }
 
 // Centrage dynamique de la carte
-map.fitBounds(bounds)
+if (bounds.length >= 2) {
+    map.fitBounds(bounds);
+}
+else {
+    map.setView(coord, 13);
+}
+
 
 
 
